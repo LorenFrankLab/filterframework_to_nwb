4a0eeaebcc14e21780c89d6079b1d9bcd51a63fb
Eric Denovellis <edeno@bu.edu>
2019-09-13 11:57:58 -0700

    Formatting

--- filterframework_to_nwb/ffnwb.py
+++ filterframework_to_nwb/ffnwb.py
@@ -61,8 +61,9 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
             location=location,
             device=recording_device)
 
-        # 2. Represent each channels of the tetrode as a row in the NWBFile.electrodes table.
-        #    We do not have x and y coordinates for electrodes, so we set to np.nan.
+        # 2. Represent each channels of the tetrode as a row in the
+        # NWBFile.electrodes table. We do not have x and y coordinates for
+        # electrodes, so we set to np.nan.
         for i in range(num_chan_per_tetrode):
             nwbf.add_electrode(x=np.nan,
                                y=np.nan,
@@ -74,15 +75,16 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
                                group=tet_electrode_group[tet_num],
                                group_name=tet_electrode_group[tet_num].name,
                                id=chan_num)
-            chan_num = chan_num + 1  # total number of channels processed so far across all tets
+            chan_num = chan_num + 1  # number of channels processed so far
 
-        # 3. Create an Electrode Table Region (slice into the electrodes table) for each tetrode
+        # 3. Create an Electrode Table Region (slice into the electrodes table)
+        # for each tetrode
         table_region_description = f'tetrode {tet_num} all channels'
         table_region_name = f'{tet_num}'
         # rows of NWBFile.electrodes table
         table_region_rows = list(
             range(chan_num - num_chan_per_tetrode, chan_num))
-        tet_electrode_table_region[tet_num] = nwbf.create_electrode_table_region(
+        tet_electrode_table_region[tet_num] = nwbf.create_electrode_table_region(  # noqa
             region=table_region_rows,
             description=table_region_description,
             name=table_region_name)
@@ -109,8 +111,8 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
     lfp = pynwb.ecephys.LFP()
 
     ""
-    # we will read in all of the timestamps and data and then combine things into a single timestamps vector
-    # and a single 2D data array
+    # we will read in all of the timestamps and data and then combine things
+    # into a single timestamps vector and a single 2D data array
     timestamps = dict()
     data = dict()
     start_time = 0
@@ -120,11 +122,13 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
         lfp_name = "{prefix}lfp-{day}".format(
             prefix=animal_prefix.lower(), day=day)
 
-        # Parse the LFP from Frank Lab Matlab files. timestamps and data will be lists of times / data points, one list per epoch
+        # Parse the LFP from Frank Lab Matlab files. timestamps and data will
+        # be lists of times / data points, one list per epoch
         timestamps[tet_num], data[tet_num] = ns.build_day_eeg(
             eeg_files[day][tet_num], eeg_samprate)
 
-    # put all of the LFP data on a common time base by truncating and interpolating where necessary
+    # put all of the LFP data on a common time base by truncating and
+    # interpolating where necessary
     first_tet = list(tetrode_metadata.keys())[0]
     num_epochs = len(timestamps[first_tet])
 
@@ -135,20 +139,24 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
     for epoch in range(num_epochs):
         # find the times for each epoch
         for tet_num in timestamps.keys():
-            # if the current start time is before the first timestamp, reset it to the first timestamp
+            # if the current start time is before the first timestamp, reset it
+            # to the first timestamp
             if start_time[epoch] < timestamps[tet_num][epoch][0]:
                 start_time[epoch] = timestamps[tet_num][epoch][0]
-            # if the current end time is after the last timestamp, reset it to the first timestamp
+            # if the current end time is after the last timestamp, reset it to
+            # the first timestamp
             if end_time[epoch] > timestamps[tet_num][epoch][-1]:
                 end_time[epoch] = timestamps[tet_num][epoch][-1]
 
-                # Given that list of start and end times, we can go through each epoch and interpolate the data to the new times
+    # Given that list of start and end times, we can go through each epoch and
+    # interpolate the data to the new times
     final_times = np.zeros((0,), np.float64)
     final_data = np.zeros((len(timestamps.keys()), 0), np.int16)
-    # final_data = np.zeros((len(data.keys()),), np.int16)
+
     for epoch in range(num_epochs):
         times = np.arange(start_time[epoch], end_time[epoch], 1 / eeg_samprate)
-        #  this allows for sample times past end_time[epoch], so we need to test for that and delete the final element in that case
+        # this allows for sample times past end_time[epoch], so we need to
+        # test for that and delete the final element in that case
         if times[-1] > end_time[epoch]:
             times = times[0:-2]
         # interpolate across the data for the new times
@@ -184,35 +192,38 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
     nwbf.add_unit_column('sorting_metric', 'a sorting metric for this unit')
 
     # ------------
-    # Load spiking data into a dictionary ordered by [tetrode num][cluster num][epoch]
+    # Load spiking data into a dictionary ordered by
+    # [tetrode num][cluster num][epoch]
     # ------------
     spiking_data = ns.parse_franklab_spiking_data(data_dir, animal_prefix, day)
 
     # ------------
-    # Add each cluster to the NWBFile.units table, concatenating the data across epochs
+    # Add each cluster to the NWBFile.units table, concatenating the data
+    # across epochs
     # and keeping track of observation intervals
     # ------------
     cluster_id = 0  # increment for each cluster we process
     for tet_num in spiking_data.keys():
         for cluster_num in spiking_data[tet_num].keys():
 
-            # Collect spike times and obs intervals for every epoch in this cluster
-            # dictionary indexed by epoch
+            # Collect spike times and obs intervals for every epoch in this
+            # cluster dictionary indexed by epoch
             cluster_spikes = spiking_data[tet_num][cluster_num]
             spike_times_each_epoch = []
             obs_int_each_epoch = np.zeros([0, 2])
             for epoch in cluster_spikes.keys():
                 # Get this cluster's spikes for this epoch
                 if cluster_spikes[epoch]['data'].shape[0]:
-                    epoch_data = cluster_spikes[epoch]['data'][:,
-                                                               timestamps_column]
+                    epoch_data = cluster_spikes[
+                        epoch]['data'][:, timestamps_column]
                     spike_times_each_epoch.append(
                         epoch_data + nwbf.session_start_time.timestamp())
                 # Get this cluster's observation intervals for this epoch
                 for epoch_obs_int in cluster_spikes[epoch]['timerange']:
                     # Convert from Nspike timestamps to POSIX time
-                    epoch_obs_int = epoch_obs_int.T.astype(
-                        float) / NSpike_timestamps_per_sec + nwbf.session_start_time.timestamp()
+                    epoch_obs_int = (epoch_obs_int.T.astype(
+                        float) / NSpike_timestamps_per_sec +
+                        nwbf.session_start_time.timestamp())
                     obs_int_each_epoch = np.append(
                         obs_int_each_epoch, [epoch_obs_int], axis=0)
 
@@ -233,7 +244,8 @@ def write_ephys(nwbf, data_dir, animal_prefix, day, recording_device):
 
 
 def write_behav(nwbf, data_dir, animal_prefix, day):
-    # write position, head_direction, speed, linear position and apparatus information
+    # write position, head_direction, speed, linear position and apparatus
+    # information
 
     position = pynwb.behavior.Position(name='Position')
     head_dir = pynwb.behavior.CompassDirection(name='Head Direction')
@@ -251,7 +263,7 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         linpos_data = ns.parse_franklab_linpos_data(
             data_dir, animal_prefix, day)
         linpos_found = True
-    except:
+    except FileNotFoundError:
         linpos_found = False
 
     # load the task information
@@ -262,7 +274,7 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
     pos_samples = np.zeros((0, 2))  # x/y positions (n x 2)
     dir_samples = np.array([])
     speed_samples = np.array([])
-    behavior_timestamps = []  # behavior timestamps are shared except for linpos
+    behavior_timestamps = []  # behavior timestamps are shared except linpos
     apparatus_dict = dict()
     statematrix_dict = dict()
 
@@ -276,9 +288,11 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         epoch_dir_samples = epoch_data['data'][:, dir_idx]
         epoch_speed_samples = epoch_data['data'][:, vel_idx] * m_per_pixel
 
-        # Timestamps for this epoch (note that we convert timestamps to POSIX time)
-        epoch_timestamps = epoch_data['data'][:,
-                                              time_idx] + nwbf.session_start_time.timestamp()
+        # Timestamps for this epoch (note that we convert timestamps to
+        # POSIX time)
+        epoch_timestamps = (
+            epoch_data['data'][:, time_idx] +
+            nwbf.session_start_time.timestamp())
 
         # Add this epoch's data to the array
         pos_samples = np.concatenate((pos_samples, epoch_pos_samples), axis=0)
@@ -288,7 +302,8 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         behavior_timestamps = np.concatenate(
             (behavior_timestamps, epoch_timestamps), axis=0)
 
-        # Store the times of epoch start and end. We will use these later to build the 'epochs' table
+        # Store the times of epoch start and end. We will use these later to
+        # build the 'epochs' table
         epoch_time_ivls.append([epoch_timestamps[0], epoch_timestamps[-1]])
 
         # parse the task information structure
@@ -302,10 +317,12 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
             continue
 
         if linpos_found and (task_data['type'][0] != 'sleep'):
-            # for non-sleep box epochs we also need to process the linear position from the statematrix element
+            # for non-sleep box epochs we also need to process the linear
+            # position from the statematrix element
             statematrix = linpos_data[epoch_num]['statematrix']
-            # for each field in the statematrix we create a separate timeseries object, and here we first
-            # concatenate the data. There is probably a better way to do this.
+            # for each field in the statematrix we create a separate timeseries
+            # object, and here we first concatenate the data. There is probably
+            # a better way to do this.
             for key in list(statematrix.keys()):
                 if key not in statematrix_dict:
                     statematrix_dict[key] = statematrix[key]
@@ -315,21 +332,24 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
 
     # adjust the statematrix timestamps
     if linpos_found:
-        statematrix_dict['time'] = np.ndarray.flatten(np.asarray(statematrix_dict['time'] +
-                                                                 nwbf.session_start_time.timestamp()))
+        statematrix_dict['time'] = np.ndarray.flatten(
+            np.asarray(statematrix_dict['time'] +
+                       nwbf.session_start_time.timestamp()))
 
     # Add the across-epochs behavioral data to the PyNWB objects
-    # See place_field_with_queries.ipynb for examples of how we query these for specific epochs
+    # See place_field_with_queries.ipynb for examples of how we query these for
+    # specific epochs
     position.create_spatial_series(name='Position',
                                    timestamps=behavior_timestamps,
                                    data=pos_samples,
                                    reference_frame='corner of video frame')
 
-    head_dir.create_spatial_series(name='Head direction',
-                                   timestamps=behavior_timestamps,
-                                   data=dir_samples,
-                                   reference_frame='0=direction of top of video frame; ' +
-                                                   'positive values clockwise (need to confirm this)')
+    head_dir.create_spatial_series(
+        name='Head direction',
+        timestamps=behavior_timestamps,
+        data=dir_samples,
+        reference_frame=('0=direction of top of video frame; '
+                         'positive values clockwise (need to confirm this)'))
 
     speed.create_timeseries(name='Speed',
                             timestamps=behavior_timestamps,
@@ -342,11 +362,12 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         if key == 'time':
             continue
         elif linpos_found:
-            linpos.create_timeseries(name=key,
-                                     timestamps=statematrix_dict['time'],
-                                     data=statematrix_dict[key],
-                                     unit='various',
-                                     description='linear position statematrix information')
+            linpos.create_timeseries(
+                name=key,
+                timestamps=statematrix_dict['time'],
+                data=statematrix_dict[key],
+                unit='various',
+                description='linear position statematrix information')
 
     # create a ProcessingModule for behavior data
     behav_mod = nwbf.create_processing_module(name='Behavior',
@@ -367,20 +388,23 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         if 'type' not in task_data.keys():
             continue
         # create the necessary apparati.
-        # NOTE that in reality the same track has slightly different linear coordinates in each epoch,
-        # but for simplicity we only take the one from the first epoch for each track. As we are also saving the
+        # NOTE that in reality the same track has slightly different linear
+        # coordinates in each epoch, but for simplicity we only take the one
+        # from the first epoch for each track. As we are also saving the
         # linearized position this should be okay.
         if task_data['type'][0] == 'sleep':
             if 'Sleep Box' not in apparatus_dict.keys():
-                # for sleep epochs we do not create an polygon, so we just create an empty apparatus object
+                # for sleep epochs we do not create an polygon, so we just
+                # create an empty apparatus object
                 apparatus_dict['Sleep Box'] = fle.Apparatus(
                     name='Sleep Box'.format(epoch_num), nodes=[], edges=[])
         else:
             apparatus_name = task_data['environment'][0]
             if apparatus_name not in apparatus_dict.keys():
-                apparatus_dict[apparatus_name] = flh.get_apparatus_from_linpos(linpos_data[epoch_num],
-                                                                               name=apparatus_name,
-                                                                               conversion=m_per_pixel)
+                apparatus_dict[apparatus_name] = flh.get_apparatus_from_linpos(
+                    linpos_data[epoch_num],
+                    name=apparatus_name,
+                    conversion=m_per_pixel)
     # create ProcessingModules for apparatus and task data
     apparatus_mod = nwbf.create_processing_module(name='Apparatus',
                                                   description='Apparatus data')
@@ -390,12 +414,14 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         apparatus_mod.add_data_interface(apparatus_dict[apparatus])
 
     task_name = 'Sleep'
-    description = 'The animal_prefix sleeps or wanders freely around a small, empty box.'
+    description = ('The animal_prefix sleeps or wanders freely around a small,'
+                   ' empty box.')
     task_mod.add_data_interface(
         fle.Task(name=task_name, description=description))
 
     task_name = 'W-Alternation'
-    task_description = 'The animal_prefix runs in an alternating W pattern between three neighboring arms of a maze.'
+    task_description = ('The animal_prefix runs in an alternating W pattern'
+                        ' between three neighboring arms of a maze.')
     task_mod.add_data_interface(
         fle.Task(name=task_name, description=task_description))
 
@@ -414,7 +440,8 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
     # Iteratively add each epoch to the NWBFile.epochs table
     # ---------
     task_exposure_dict = dict()
-    # the no exposure start num is used for sleep epochs, and the 1000*day ensures that it's unique across days
+    # the no exposure start num is used for sleep epochs, and the 1000*day
+    # ensures that it's unique across days
     no_exposure_start_num = 1000 * day
     for epoch_num, epoch_metadata in all_epochs_taskdata.items():
         task_data = all_epochs_taskdata[epoch_num]
@@ -435,8 +462,8 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
             epoch_metadata, apparatus_mod)
 
         epoch_exposure_num = ns.get_exposure_num(epoch_metadata)
-        # if this is -1 then we'll replace it with something that starts at 1e6 so it's always positive and
-        # incrementable in a sensible way
+        # if this is -1 then we'll replace it with something that starts at 1e6
+        # so it's always positive and incrementable in a sensible way
         if epoch_exposure_num == -1:
             # check to see if this is the first time that this task
             if epoch_task in task_exposure_dict.keys():
@@ -450,8 +477,9 @@ def write_behav(nwbf, data_dir, animal_prefix, day):
         epoch_tags = ''
 
         # Add this epoch to the NWBFile.epochs table
-        # Note that task and apparatus are references to the "Behavior" ProcessingModule,
-        # so they will not be unnecessarily duplicated within the NWBFile
+        # Note that task and apparatus are references to the "Behavior"
+        # ProcessingModule, so they will not be unnecessarily duplicated within
+        # the NWBFile
         nwbf.add_epoch(start_time=epoch_start_time,
                        stop_time=epoch_stop_time,
                        exposure=task_exposure_dict[epoch_task],
@@ -471,7 +499,8 @@ def convert_to_nwb(data_dir, nwb_dir, animal_prefix, animal_name, days,
                        2006, 1, 1, 12, 0, 0, tzinfo=tz.gettz('US/Pacific')),
                    device='NSpike acquisition system',
                    experimenter='FrankLab Anonymous',
-                   experiment_description='Tetrode recordings from rat during rest and task performance',
+                   experiment_description=('Tetrode recordings from rat during'
+                                           ' rest and task performance'),
                    session_description='data acquisition session'):
     '''Convert one or more days of data into NWB files.
 
@@ -498,10 +527,6 @@ def convert_to_nwb(data_dir, nwb_dir, animal_prefix, animal_name, days,
         # process the data for each day
         nwb_file_name = os.path.join(
             nwb_dir, f'{animal_prefix.lower()}{day:02d}.nwb')
-        # check to see if the file exists
-        assert(not os.path.exists(nwb_file_name) or overwrite), f"Error: NWB file {nwb_file_name} already exists. Use " \
-            "overwrite=True to replace existing " \
-            "file"
 
         logger.info(f'Converting {animal_name} day {day}')
 
